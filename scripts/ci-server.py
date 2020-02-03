ec65d8822428b9c0cf541f37475e8c0e70edfd6b
Leonardo Robol <leo@robol.it>
2020-02-03 16:30:09 +0100

    Draft implementation of CI server.

---
+++ scripts/ci-server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#
+
+#
+# CI server that automatically updates the code when
+# it is pushed to the Github repository. 
+#
+
+# Run this application with:
+#
+# gunicorn ci-server:app -b 0.0.0.0:8081
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def update_caps():
+    # Run the update command, and leave it running
+    os.system("flock /tmp/caps.lock -c 'update-caps' &")
+
+    return "Update triggered"
+
+    
