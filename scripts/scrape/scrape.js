db6d3e89d36919af24e94e44a3b8bd5312975ae1
Emanuele Paolini <emanuele.paolini@gmail.com>
2020-02-04 17:27:51 +0100

    lo script ora è in grado di stampare i pdf dal vecchio sito di caps. Ma io posso stampare solamente il mio stesso piano di studi.

--- scripts/scrape/scrape.js
+++ scripts/scrape/scrape.js
@@ -3,21 +3,53 @@ SETUP:
 npm install
 
 RUN:
-node scrape.js 
+node scrape.js
 **/
 
 const puppeteer = require('puppeteer');
-const URL = 'http://caps.dm.unipi.it/develop';
+const readline = require('readline');
+
+function prompt(rl, question) {
+  return new Promise(function(resolve, reject) {
+    var ask = function() {
+      rl.question(question, function(answer) {
+          resolve(answer, reject);
+      });
+    };
+		ask();
+  });
+};
+
+const URL = 'https://www.dm.unipi.it/caps';
 async function run () {
+		const rl = readline.createInterface({
+		  input: process.stdin,
+		  output: process.stdout
+		});
+
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(URL);
-		await page.type('#username', 'xleonardo');
-		await page.type('#password', 'xleonardo');
+		var username = await prompt(rl, 'Username [' + URL + ']: ');
+		var password = await prompt(rl, 'Password: ');
+		await page.type('#UserUsername', username);
+		await page.type('#UserPassword', password);
 		await page.click('[value="Login"]');
+		const element = await page.$("#flashMessage");
+		if (element) {
+			const text = await page.evaluate(element => element.textContent, element);
+			console.log("error: " + text);
+			browser.close();
+			return
+		}
+		await page.screenshot({path: 'screenshot.png'});
+		console.log("writing screenshot");
 		// await page.waitForNavigation();
-		await page.goto(URL + '/proposals/view/956');
-    await page.pdf({path: 'out.pdf'});
-    browser.close();
+		var id = await prompt(rl, 'Proposal id (try 723): ');
+		rl.close();
+		await page.goto(URL + '/proposals/view/' + id);
+		console.log("writing pdf");
+    await page.pdf({path: 'proposal_' + id + '.pdf'});
+		browser.close();
 }
 run();
