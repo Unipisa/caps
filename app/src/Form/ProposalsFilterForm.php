d36d07db8c7a812400cbe11683f74b26c9cbeb34
Emanuele Paolini <emanuele.paolini@gmail.com>
2020-02-18 15:23:03 +0100

    refactoring ProposalFilterForm. Aggiunto campo "curriculum" al filtro.

--- app/src/Form/ProposalsFilterForm.php
+++ app/src/Form/ProposalsFilterForm.php
@@ -7,13 +7,20 @@ use Cake\Validation\Validator;
 
 class ProposalsFilterForm extends Form
 {
+    public function __construct($query) {
+      parent::__construct();
+      $this->query = $query;
+    }
+
     protected function _buildSchema(Schema $schema)
     {
         return $schema
 				  ->addField('status', ['type' => 'select', 'options' => ['pippo', 'pluto', 'topolino']])
 					->addField('surname', ['type' => 'string'])
           ->addField('academic_year', ['type' => 'string'])
-          ->addField('degree', ['type' => 'string', 'label' => __('laurea')]);
+          ->addField('degree', ['type' => 'string'])
+          ->addField('curriculum', ['type' => 'string'])
+          ;
     }
 
     public function validationDefault(Validator $validator)
@@ -24,38 +31,42 @@ class ProposalsFilterForm extends Form
         return $validator;
     }
 
-    public function filterProposals($proposals) {
+    public function filterFieldLike($dbfield, $field) {
+        if (!empty($this->getData($field))) {
+          $this->query = $this->query->where([
+            $dbfield.' LIKE' => '%'.$this->getData($field).'%'
+          ]);
+        }
+      }
+
+    public function filterFieldEqual($dbfield, $field) {
+        if (!empty($this->getData($field))) {
+          $this->query = $this->query->where([
+            $dbfield => $this->getData($field)
+          ]);
+        }
+    }
+
+    protected function _execute(array $data) {
+      $this->setData($data);
       if ($this->getData('status') == 'pending') {
-        $proposals = $proposals->where([
+        $this->query = $this->query->where([
             'Proposals.submitted' => true,
             'Proposals.approved' => false
         ]);
       } else if ($this->getData('status') == 'approved') {
-        $proposals = $proposals->where([
+        $this->query = $this->query->where([
           'Proposals.approved' => true,
           'Proposals.frozen' => false ]);
       } else if ($this->getData('status') == 'archived') {
-        $proposals = $proposals->where([
+        $this->query = $this->query->where([
           'Proposals.frozen' => true ]);
       }
-      if (!empty($this->getData('surname'))) {
-        $proposals = $proposals->where([
-          'Users.surname LIKE' => '%'.$this->getData('surname').'%'
-        ]);
-      }
-      if (!empty($this->getData('academic_year'))) {
-        // Error: SQLSTATE[HY000]: General error: 1 no such column: Curricula.academic_year
-        $proposals = $proposals->where([
-          'Curricula.academic_year' => $this->getData('academic_year')
-        ]);
-      }
-      if (!empty($this->getData('degree'))) {
-        // Error: Error: SQLSTATE[HY000]: General error: 1 no such column: Degree.name
-        $proposals = $proposals->where([
-          'Degrees.name LIKE' => '%'.$this->getData('degree').'%'
-        ]);
-      }
-      return $proposals;
+      $this->filterFieldLike('Users.surname', 'surname');
+      $this->filterFieldEqual('Curricula.academic_year', 'academic_year');
+      $this->filterFieldLike('Degrees.name', 'degree');
+      $this->filterFieldLike('Curricula.name', 'curriculum');
+      return $this->query;
     }
 }
 ?>
