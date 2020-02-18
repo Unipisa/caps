c24d9ae1bd99582581822e7399a0090df03b4aaf
Emanuele Paolini <emanuele.paolini@gmail.com>
2020-02-18 12:54:32 +0100

    migrazioni per aggiungere la foreignkey curriculum_id ai proposals

--- app/config/Migrations/20200218092832_RemoveInvalidProposals.php
+++ app/config/Migrations/20200218092832_RemoveInvalidProposals.php
@@ -10,9 +10,16 @@ class RemoveInvalidProposals extends AbstractMigration
      * http://docs.phinx.org/en/latest/migrations.html#the-change-method
      * @return void
      */
-    public function change()
+    public function up()
     {
       // Migrate the current data
+      // in sqlite you should fake this migration:
+      // bin/cake migrations mark_migrated -o -t 20200218092832
       $this->execute('delete proposals from proposals left join curricula_proposals as cp on proposals.id=cp.proposal_id where cp.id is null;');
+    }
+
+    public function down()
+    {
+      // Cannot recover invalid data
     }
 }
