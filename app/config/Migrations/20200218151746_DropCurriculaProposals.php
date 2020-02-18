0f82615310ce4327c8b4518a3b1237e44da2b970
Emanuele Paolini <emanuele.paolini@gmail.com>
2020-02-18 16:41:44 +0100

    not null curriculum_id in tabella proposals

--- app/config/Migrations/20200218151746_DropCurriculaProposals.php
+++ app/config/Migrations/20200218151746_DropCurriculaProposals.php
@@ -3,16 +3,27 @@ use Migrations\AbstractMigration;
 
 class DropCurriculaProposals extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * More information on this method is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
-     * @return void
-     */
-    public function change()
+    public function up()
     {
         $table = $this->table('curricula_proposals');
-        $table->drop();
+        $table->drop()->save();
+    }
+
+    public function down()
+    {
+        $table = $this->table('curricula_proposals');
+        $table->addColumn('curriculum_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('proposal_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addForeignKey('proposal_id', 'proposals', 'id');
+        $table->addForeignKey('curriculum_id', 'curricula', 'id');
+        $table->create();
     }
 }
