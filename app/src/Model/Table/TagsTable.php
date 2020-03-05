60bb2ecea4029c47bde59f8f87096245749b781f
Leonardo Robol <leo@robol.it>
2020-03-05 15:59:22 +0100

    Added missing file.

---
+++ app/src/Model/Table/TagsTable.php
@@ -0,0 +1,65 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Tags Model
+ *
+ * @property \App\Model\Table\ExamsTable&\Cake\ORM\Association\BelongsToMany $Exams
+ *
+ * @method \App\Model\Entity\Tag get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Tag newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Tag[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Tag|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Tag saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Tag patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Tag[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Tag findOrCreate($search, callable $callback = null, $options = [])
+ */
+class TagsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('tags');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+
+        $this->belongsToMany('Exams', [
+            'foreignKey' => 'tag_id',
+            'targetForeignKey' => 'exam_id',
+            'joinTable' => 'tags_exams'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('name')
+            ->maxLength('name', 255)
+            ->allowEmptyString('name');
+
+        return $validator;
+    }
+}
