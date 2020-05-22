496547b3363f3551c2d5babdba5cf5a0abbbe683
Leonardo Robol <leo@robol.it>
2020-05-22 20:05:52 +0200

    Added missing migration file.

---
+++ app/config/Migrations/20200522165507_AddCommentToAttachments.php
@@ -0,0 +1,49 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddCommentToAttachments extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        $table = $this->table('attachments');
+
+        $table->addColumn('comment', 'text', [
+            'null' => true,
+            'default' => null
+        ]);
+
+        $table->addColumn('created', 'datetime', [
+            'null' => true,
+            'default' => null
+        ]);
+
+        $table->changeColumn('filename', 'string', [
+            'null' => true,
+            'default' => null
+        ]);
+
+        $table->update();
+    }
+
+    public function down() {
+        $table = $this->table('attachments');
+
+        $table->changeColumn('filename', 'string', [
+            'null' => false,
+            'default' => null
+        ]);
+
+        $table->removeColumn('created');
+        $table->removeColumn('comment');
+
+        $table->update();
+
+    }
+}
