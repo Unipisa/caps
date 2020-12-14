608efd0c5b916abe5ea866d9ace4065ad17b98ec
Leonardo Robol <leo@robol.it>
2020-12-14 14:03:56 +0100

    Add comment to CAPS version variable.

--- app/src/Application.php
+++ app/src/Application.php
@@ -40,6 +40,7 @@ use Psr\Http\Message\ServerRequestInterface;
  */
 class Application extends BaseApplication
 {
+    // Current CAPS version. This number is displayed in the web interface.
     public static $_CAPSVERSION = '2.1.1';
 
     /**
