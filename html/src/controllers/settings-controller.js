bebfde2a89a5f0cf2cdcd514f311928cf03ef4f6
Leonardo Robol <leo@robol.it>
2021-04-19 14:56:54 +0200

    Avoid jQuery in settings/index.

--- html/src/controllers/settings-controller.js
+++ html/src/controllers/settings-controller.js
@@ -1,23 +1,21 @@
 const CapsAppController = require("./app-controller");
-const jQuery = require('jquery');
 const ClassicEditor = require('@ckeditor/ckeditor5-build-classic');
 
 class CapsSettingsController extends CapsAppController {
 
     index() {
-        jQuery('.caps-settings-html').each(function (idx, el) {
-            ClassicEditor
-                .create(el, {
-                  toolbar: [ 'undo', 'redo', '|', 'bold', 'italic', 'link',
-                             'bulletedList', 'numberedList',  ]
-                })
-                .catch(error => {
-                  console.log('CAPS::CKEditor Error while loading the CKEditor component:');
-                  console.error(error);
-                });
-            });
+      for (const el of document.getElementsByClassName('caps-settings-html')) {
+        ClassicEditor
+          .create(el, {
+            toolbar: [ 'undo', 'redo', '|', 'bold', 'italic', 'link',
+                        'bulletedList', 'numberedList',  ]
+          })
+          .catch(error => {
+            console.log('CAPS::CKEditor Error while loading the CKEditor component:');
+            console.error(error);
+          });
+      }
     }
-
 }
 
 module.exports = CapsSettingsController;
