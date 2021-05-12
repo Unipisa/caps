73ad29fc8e87cf815c44f981b11b5747e2ad23ae
Leonardo Robol <leo@robol.it>
2021-05-12 13:13:11 +0200

    Bump version to 2.1.11

--- app/src/Application.php
+++ app/src/Application.php
@@ -41,7 +41,7 @@ use Psr\Http\Message\ServerRequestInterface;
 class Application extends BaseApplication
 {
     // Current CAPS version. This number is displayed in the web interface.
-    public static $_CAPSVERSION = '2.1.10';
+    public static $_CAPSVERSION = '2.1.11';
 
     /**
      * application version number
