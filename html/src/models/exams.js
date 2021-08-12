7eac9a8f64630d7e28543a97ee7e3a71285eed92
Leonardo Robol <leo@robol.it>
2021-08-12 10:40:34 +0200

    Add experimental cache layer for JS components.

--- html/src/models/exams.js
+++ html/src/models/exams.js
@@ -1,13 +1,20 @@
 'use strict';
 
+const Cache = require('../modules/cache');
+
 class Exams {
 
-    async all() {
-        const response = await (
-            await fetch(Caps.root + 'exams.json')
-        ).json();
+    cache = new Cache();
 
-        return response["exams"];
+    async all() {
+        const exams = this.cache.get("exams-all", async () => {
+            const response = await (
+                await fetch(Caps.root + 'exams.json')
+            ).json();
+            return response["exams"];
+        });
+
+        return exams;
     }
 
 }
--- html/src/models/groups.js
+++ html/src/models/groups.js
@@ -1,21 +1,33 @@
 'use strict';
 
+const Cache = require('../modules/cache');
+
 class Groups {
 
-    async all() {
-        const response = await (
-            await fetch(Caps.root + 'groups.json')
-        ).json();
+    cache = new Cache();
 
-        return response["groups"];
+    async all() {
+        const groups = this.cache.get("groups-all", async () => {
+            const response = await (
+                await fetch(Caps.root + 'groups.json')
+            ).json();
+    
+            return response["groups"];
+        });
+
+        return groups;
     }
 
     async get(id) {
-        const response = await (
-            await fetch(Caps.root + 'groups/view/' + id + '.json')
-        ).json();
+        const group = this.cache.get("group/" + id, async () => {
+            const response = await (
+                await fetch(Caps.root + 'groups/view/' + id + '.json')
+            ).json();
+    
+            return response;
+        });
 
-        return response;
+        return group;
     }
 
 }
