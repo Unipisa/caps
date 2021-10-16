5e0a02591981dcd17e02da0b98d1c6ba550aab90
Emanuele Paolini <emanuele.paolini@gmail.com>
2021-10-16 11:24:24 +0200

    fix tests

--- app/tests/Fixture/FormTemplatesFixture.php
+++ app/tests/Fixture/FormTemplatesFixture.php
@@ -25,6 +25,7 @@ class FormTemplatesFixture extends TestFixture
     // phpcs:disable
     public $fields = [
         'id' => ['type' => 'integer', 'length' => null, 'unsigned' => false, 'null' => false, 'default' => null, 'autoIncrement' => true, 'precision' => null, 'comment' => null],
+        'enabled' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => '1', 'precision' => null, 'comment' => null],
         'name' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'precision' => null, 'comment' => null, 'collate' => null],
         'text' => ['type' => 'text', 'length' => null, 'null' => false, 'default' => null, 'precision' => null, 'comment' => null, 'collate' => null],
         '_constraints' => [
@@ -42,6 +43,7 @@ class FormTemplatesFixture extends TestFixture
         $this->records = [
             [
                 'id' => 1,
+                'enabled' => true,
                 'name' => 'Lorem ipsum dolor sit amet',
                 'text' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
             ],
