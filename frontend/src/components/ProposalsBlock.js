8b9dc997ce46a637d31feb7dba9805c628e00e42
Leonardo Robol <leo@robol.it>
2022-03-29 07:59:38 +0200

    Split some parts out of UserProfile.js

---
+++ frontend/src/components/ProposalsBlock.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import ProposalInfo from "./ProposalInfo";
+import LoadingMessage from "./LoadingMessage";
+
+function ProposalsBlock(props) {
+    return <div className={props.className}>
+        <h2 className="d-flex">
+            <span className="mr-auto">Piani di studio</span>
+            <a href={props.root + 'proposals/edit'} className="my-auto btn btn-sm btn-primary shadow-sm">
+                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                <span className="d-none d-md-inline ml-2">Nuovo piano di studio</span>
+            </a>
+        </h2>
+        { props.proposals === undefined && <LoadingMessage>Caricamento dei piani in corso</LoadingMessage>}
+        { (props.proposals !== undefined && props.proposals.length == 0) && <p>
+            Nessun piano di studio presentato.
+            </p> }
+        { props.proposals !== undefined && 
+        <div className="row">
+            { props.proposals.map(
+                p => <ProposalInfo root={props.root} csrfToken={props.csrfToken} 
+                    key={"proposal-info-" + p.id} proposal={p}
+                    onDeleteClicked={props.onProposalDeleteClicked}>
+                </ProposalInfo>
+            )
+            }
+        </div>
+        }
+    </div>;
+}
+
+export default ProposalsBlock;
