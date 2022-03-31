5dfdb1980040e4186cdd7ddd0db872a0bf2ac5c2
Emanuele Paolini <emanuele.paolini@gmail.com>
2022-03-31 09:48:53 +0200

    progressing in react forms list management

--- frontend/src/components/Forms.js
+++ frontend/src/components/Forms.js
@@ -7,13 +7,148 @@ import FilterButton from './FilterButton';
 import LoadingMessage from './LoadingMessage';
 import FormBadge from './FormBadge';
 
+function ActionButton(props) {
+    return <div className="dropdown">
+    <button type="button" className="btn btn-sm btn-primary dropdown-toggle" id="dropDownActions"
+            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Azioni
+    </button>
+    <div className="dropdown-menu p-2 shadow" style={{"width": "450px"}}>
+        <button className="my-1 btn btn-success" style={{"width": "100%"}}
+                onClick={() =>
+                    confirm('Confermi di voler approvare i moduli selezionati?') 
+                    && props.Forms.perform_action('approve')}>
+            ✓ Approva i moduli selezionati
+        </button>
+        <button className="my-1 btn btn-danger" style={{"width": "100%"}}
+                onClick={() =>
+                    confirm('Confermi di voler rifiutare i moduli selezionati?')
+                    && props.Forms.perform_action('reject')}>
+            ✗ Rifiuta i moduli selezionati
+        </button>
+        <button className="my-1 btn btn-warning" style={{"width": "100%"}}
+                onClick={() =>
+                    confirm('Confermi di voler riportare in valutazione i moduli selezionati?')
+                    && props.Forms.perform_action('resubmit')}>
+            ⎌ Riporta in valutazione i moduli selezionati
+        </button>
+        <button className="my-1 btn btn-warning" style={{"width": "100%"}}
+                onClick={() =>
+                    confirm('Confermi di voler riportare in bozza i moduli selezionati?')
+                    && props.Forms.perform_action('redraft')}>
+            ⎌ Riporta in bozza i moduli selezionati
+        </button>
+        <button className="my-1 btn btn-danger" style={{"width": "100%"}}
+                onClick={() =>
+                    confirm('Confermi di voler eliminare i moduli selezionati?')
+                    && props.Forms.perform_action('delete')}> 
+            🗑 Elimina i moduli selezionati
+        </button>
+    </div>
+</div>
+}
+
+function HeadPanel(props) {
+    return <div className="d-flex mb-2">
+        <FilterButton                   
+            items={{
+                state: {
+                    label: "stato",
+                    type: "select",
+                    options: {
+                        '': 'tutti',
+                        'draft': 'bozze',
+                        'submitted': 'da valutare',
+                        'approved': 'approvati',
+                        'rejected': 'rifiutati'
+                    }
+                },
+                surname: "cognome",
+                formTemplate: "modello",
+                name: "nome"
+            }}
+            callback={filter_button => this.update(filter_button.filter)}>
+        </FilterButton>
+        <ActionButton Forms={Forms} />
+
+        <div className="flex-fill"></div>
+
+        <div className="col-auto">
+            <button type="button" className="btn btn-sm btn-primary" onClick={() => Forms.downloadCSV()}>
+                <i className="fas fw fa-download"></i><span className="ml-2 d-none d-md-inline">Esporta in CSV</span>
+            </button>
+        </div>
+        </div>;
+}
+
+function FormRow(props) {
+    const selected = props.Forms.state.selected.includes(props.form);
+    const form = props.form;
+    return <tr>
+        <td>
+            { selected 
+                ? <input type="checkbox" checked="checked" onClick={() => props.Forms.removeFromSelection(form)}/>
+                : <input type="checkbox" onClick={() => props.Forms.addToSelection(form)}/>
+            }
+        </td>
+        <td><FormBadge form={form}></FormBadge></td>
+        <td>{form.user.givenname+" "+form.user.surname}</td>
+        <td>{form.form_template.name}</td>
+        <td>{form.date_submitted}</td>
+        <td>{form.date_managed}</td>
+        <td>
+            <div className="d-none d-xl-inline-flex flex-row align-items-center">
+                <a href={`${props.Forms.props.root}forms/view/${form.id}`}>
+                    <button type="button" className="btn btn-sm btn-primary mr-2">
+                    <i className="fas fa-eye mr-2"></i>
+                    Visualizza
+                    </button>
+                </a>
+            </div>
+        </td>
+    </tr>
+}
+
 class Forms extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            'forms': undefined
+            'forms': undefined,
+            'selected': []
         };
         this.update(this.props.query || {});
+    }
+
+    addToSelection(form) {
+        if (!this.state.forms || !this.state.forms.includes(form) || this.state.selected.includes(form)) {
+            console.log("cannot select form");
+            return;
+        }
+        this.setState({
+            "selected": this.state.selected.concat([form]) 
+        })
+    }
+
+    removeFromSelection(form) {
+        if (!this.state.forms || !this.state.forms.includes(form) || !this.state.selected.includes(form)) {
+            console.log("cannot deselect form");
+            return;
+        }
+        this.setState({
+            "selected": this.state.selected.filter(f => f!=form)
+        });
+    }
+
+    async perform_action(action, selected) {
+        if (!["approve", "reject", "redraft", "delete"].includes(action)) {
+            console.log("ERROR: invalid action");
+            return;
+        }
+        // to be implemented...
+    }
+
+    downloadCSV() {
+        // to be implemented...
     }
 
     async update(query) {
@@ -26,30 +161,6 @@ class Forms extends React.Component {
     }
 
     onDeleteClicked(f) {
-    }
-
-    renderHeadPanel() {
-        return <div className="d-flex mb-2">
-            <FilterButton                   
-                items={{
-                    state: {
-                        label: "stato",
-                        type: "select",
-                        options: {
-                            '': 'tutti',
-                            'draft': 'bozze',
-                            'submitted': 'da valutare',
-                            'approved': 'approvati',
-                            'rejected': 'rifiutati'
-                        }
-                    },
-                    surname: "cognome",
-                    formTemplate: "modello",
-                    name: "nome"
-                }}
-                callback={filter_button => this.update(filter_button.filter)}>
-            </FilterButton>
-        </div>;
     }
 
     renderTailPanel() {
@@ -75,24 +186,13 @@ class Forms extends React.Component {
                         </tr>
                     </thead>
                     <tbody>
-                    {this.state.forms.map(form => <tr key={form.id}>
-                        <td><input type="checkbox" value={form.id}></input></td>
-                        <td><FormBadge form={form}></FormBadge></td>
-                        <td>{form.user.givenname+" "+form.user.surname}</td>
-                        <td>{form.form_template.name}</td>
-                        <td>{form.date_submitted}</td>
-                        <td>{form.date_managed}</td>
-                        <td>
-                            <div className="d-none d-xl-inline-flex flex-row align-items-center">
-                                <a href={`${this.props.root}forms/view/${form.id}`}>
-                                    <button type="button" className="btn btn-sm btn-primary mr-2">
-                                    <i className="fas fa-eye mr-2"></i>
-                                    Visualizza
-                                    </button>
-                                </a>
-                            </div>
-                        </td>
-                    </tr>)}
+                    {this.state.forms.map(form => 
+                        <FormRow 
+                            key={form.id} 
+                            form={form} 
+                            Forms={this}
+                            >
+                        </FormRow>)}
                     </tbody>
                 </table>
             </div>
@@ -103,7 +203,7 @@ class Forms extends React.Component {
         return <div>
             <h1>Moduli</h1>
             <Card>
-                { this.renderHeadPanel() }
+                <HeadPanel Forms={this}></HeadPanel>
                 { this.renderTable() }
                 { this.renderTailPanel() }
             </Card>
