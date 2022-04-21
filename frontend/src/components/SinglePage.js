65ce21a4af054e6cee0e35615378a42d06cb4226
Emanuele Paolini <emanuele.paolini@gmail.com>
2022-04-21 17:08:42 +0200

    aggiunta barra headers e footers

--- frontend/src/components/SinglePage.js
+++ frontend/src/components/SinglePage.js
@@ -14,21 +14,95 @@ class SinglePage extends React.Component {
     }
 
     render() {
+        const capsVersion="vv.ww.xx";
+
         return <>
-            <NavBar />
+        <div id="wrapper">
+            <NavBar capsVersion= { capsVersion }/>
+            <div id="content-wrapper" className="d-flex flex-column">
+                <TopBar />
+                <div id="content">
+                    SinglePage!
+                </div>
+                <Footer capsVersion={ capsVersion }/>
+            </div>
+        </div>
         </>
     }
 }
 
 export default SinglePage;
 
-function NavBar(props) {
+function TopBar(props) {
+    return <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+    
+    <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
+        <svg className="svg-inline--fa fa-bars fa-w-14" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
+    </button>
+
+        <div className="rounded text-primary border-left-warning px-2 py-1 my-auto text-sm-left text-wrap">
+                        <p>Messaggio pinco pallino</p>    </div>
+    
+    
+    <ul className="navbar-nav ml-auto">
+
+        <div className="d-none d-md-block nav-item my-auto">
+            <img src="/img/logo_blue_small.png" alt=""/>        
+        </div>
+
+        
+            <div className="topbar-divider d-none d-sm-block"></div>
+
+            
+            <li className="nav-item dropdown no-arrow">
+                <a className="nav-link dropdown-toggle text-primary" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span className="mr-2 d-none d-lg-inline text-primary small">Pippo Ginnasta</span>
+                    <svg className="svg-inline--fa fa-user fa-w-14 fa-lg mx-2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path></svg>
+                </a>
+                
+                <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                    <div className="dropdown-item">
+                        Collegato come <strong>USERNAME</strong> (amministratore?!?)
+                    </div>
+                    <div className="dropdown-divider"></div>
+
+                                            <a className="dropdown-item" href="/users/view/723">
+                            <svg className="svg-inline--fa fa-user fa-w-14 fa-sm fa-fw mr-2 text-gray-400" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path></svg>
+                            I miei documenti
+                        </a>
+                        <div className="dropdown-divider"></div>
+                            <a className="dropdown-item" href="/users/logout">
+                            <svg className="svg-inline--fa fa-user fa-w-14 fa-lg mx-2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
+                                <path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path>
+                            </svg>Logout
+                    </a>
+                </div>
+            </li>
+        
+    </ul>
+</nav>
+}
+
+function Footer({ capsVersion }) {
+    return <footer className="sticky-footer bg-white">
+    <div className="container my-auto">
+        <div className="copyright text-center my-auto">
+            <span>Copyright © Dipartimento di Matematica — Università di Pisa — 2021 2022</span>
+            <div className="mx-4 d-inline"></div>
+            <span>CAPS version { capsVersion }</span>
+        </div>
+    </div>
+</footer>
+}
+
+function NavBar({ capsVersion }) {
     return <ul className="navbar-nav bg-primary sidebar sidebar-dark accordion" id="accordionSidebar">
     <a className="sidebar-brand d-flex align-items-center justify-content-center" href="#">
         <div className="sidebar-brand-icon">
             <img src="/img/cherubino_white.png" className="mx-1" />
         </div>
-        <div className="sidebar-brand-text mx-3">CAPS<sup>2.8.5</sup></div>
+        <div className="sidebar-brand-text mx-3">CAPS<sup>{ capsVersion }</sup></div>
     </a>
     <div className="d-flex justify-content-center">
         <div className="text-white text-uppercase font-weight-bold my-2 mx-2 px-2" style={{fontSize: "0.7rem"}}>
