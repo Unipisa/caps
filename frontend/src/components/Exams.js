4c9ea4d2b0e436ca53d22f5f0b22f72229b1b6d6
Emanuele Paolini <emanuele.paolini@gmail.com>
2022-04-21 18:57:28 +0200

    aggiunto router alla single page app

---
+++ frontend/src/components/Exams.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import React from 'react';
+import RestClient from '../modules/api';
+
+class Exams extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+    }
+
+    render() {
+        return <>
+            <h1>Esami</h1>
+    </>
+    }
+
+}
+
+export default Exams;
