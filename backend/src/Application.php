7d2dfdda2bdb3b86473d674cc170fd181f2bdc68
Leonardo Robol <leo@robol.it>
2022-06-22 08:59:33 +0200

    CAPS v2.9.7

--- backend/src/Application.php
+++ backend/src/Application.php
@@ -50,7 +50,7 @@ use Psr\Http\Message\ServerRequestInterface;
 class Application extends BaseApplication implements AuthenticationServiceProviderInterface
 {
     // Current CAPS version. This number is displayed in the web interface.
-    public static $_CAPSVERSION = '2.9.6';
+    public static $_CAPSVERSION = '2.9.7';
 
     /**
      * application version number
