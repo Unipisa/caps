027761f0e4beb04c48ae4da8001e6691e21a219d
Leonardo Robol <leo@robol.it>
2022-07-13 17:36:14 +0200

    CAPS v2.9.11
     * Fix link on share proposal emails.
     * Fix spacing in the admin dashboard.

--- backend/src/Application.php
+++ backend/src/Application.php
@@ -50,7 +50,7 @@ use Psr\Http\Message\ServerRequestInterface;
 class Application extends BaseApplication implements AuthenticationServiceProviderInterface
 {
     // Current CAPS version. This number is displayed in the web interface.
-    public static $_CAPSVERSION = '2.9.10';
+    public static $_CAPSVERSION = '2.9.11';
 
     /**
      * application version number
