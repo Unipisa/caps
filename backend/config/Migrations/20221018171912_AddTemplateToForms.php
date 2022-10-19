a5b51c56211f56a869b36d1b8729b96f39c2ee6f
Leonardo Robol <leo@robol.it>
2022-10-19 17:59:19 +0200

    Added missing migration.

---
+++ backend/config/Migrations/20221018171912_AddTemplateToForms.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class AddTemplateToForms extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        $table = $this->table('forms');
+
+        $table->addColumn('template_text', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+
+        $table->update();
+
+        // FIXME: This does not work on SQLite
+        $this->execute('UPDATE forms, form_templates set forms.template_text=form_templates.text WHERE forms.form_template_id=form_templates.id');
+    }
+
+    public function down() 
+    {
+        $table = $this->table('forms');
+        $table->removeColumn('template_text');
+        $table->update();
+    }
+}
