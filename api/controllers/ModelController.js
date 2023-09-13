300f1becf5f51adb4dac3be022ed50264f91056d
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-09-13 10:48:27 +0200

    migliorata creazione della pipeline e resa una funzione riutilizzabile

--- api/controllers/ModelController.js
+++ api/controllers/ModelController.js
@@ -6,93 +6,106 @@
 const { default: mongoose } = require("mongoose");
 const { BadRequestError, ValidationError, NotImplementedError } = require("../exceptions/ApiException");
 
+function queryFieldsToPipeline(query, fields) {
+    let $match = {};
+    let filter = {};
+    let sort = "_id";
+    let direction = 1;
+    let limit = 100;
+
+    for (key in query) {
+        const value = query[key];
+        if (key == '_direction') {
+            if (value=="1") direction = 1;
+            else if (value=="-1") direction = -1;
+            else throw new BadRequestError(`invalid _direction ${value}: 1 or -1 expected`);
+        } else if (key == '_limit') {
+            limit = parseInt(value);
+            if (isNaN(limit) || limit < 0) throw BadRequestError(`invalid _limit ${value}: positive integer expected`);
+        } else if (key == '_sort') {
+            if (!(fields[value] && fields[value].can_sort)) {
+                throw new BadRequestError(`invalid _sort key ${value}. Fields: ${ JSON.stringify(fields) }`);
+            }
+            sort = value;
+        } else if (fields[key] && fields[key].can_filter) {
+            const field = fields[key];
+            filter[key] = value;
+            if (field.match_regex) {
+                $match[key] = field.match_regex(value)
+            } else if (field.match_integer) {
+                $match[key] = parseInt(value);
+            } else if (field.match_ids) {
+                $match['_id'] = {
+                    $in: value.split(",").map(id => new mongoose.Types.ObjectId(id))
+                };
+            } else if (field.match_id_object) {
+                $match[key] = mongoose.Types.ObjectId(value);
+            } else {
+                $match[key] = value;
+            }
+        }
+    }
+
+    const $sort = {};
+    $sort[sort] = direction;
+    const pipeline = [
+        {$match},
+        {$sort},
+        {$facet:{
+            "counting": [ 
+                { "$group": {_id:null, count:{$sum:1}}},
+            ],
+            "limiting" : [ { "$skip": 0}, {"$limit": limit} ]
+        }},
+        // if the pipeline is empty, the facet returns an empty array
+        // so we need to add a dummy entry
+        {$addFields: {
+            "counting": { $cond: { if: { $eq: [ "$counting", [] ] }, then: [ {_id: null, count: 0} ], else: "$counting" } }
+        }},
+        {$unwind: {
+            "path": "$counting",
+        }},
+        {$project:{
+            total: "$counting.count",
+            items: "$limiting",
+            fields: {$literal: fields},
+            sort: {$literal: sort},
+            direction: {$literal: direction},
+            limit: {$literal: limit},
+            query: {$literal: query},
+            match: {$literal: $match},
+        }},
+    ]
+    return pipeline
+}
+
 const ModelController = {
+    queryFieldsToPipeline,
 
     /**
      * restrict: restricts the query to a subset of the collection
      *    used as {$match: restrict} in the pipeline
      */
     index: async (Model, query, fields, {  populate, restrict } = {}) => {
-        let $match = {};
-        let filter = {};
-        let sort = "_id";
-        let direction = 1;
-        let limit = 100;
+        const pipeline = queryFieldsToPipeline(query, fields)
 
-        for (key in query) {
-            const value = query[key];
-            if (key == '_direction') {
-                if (value=="1") direction = 1;
-                else if (value=="-1") direction = -1;
-                else throw new BadRequestError(`invalid _direction ${value}: 1 or -1 expected`);
-            } else if (key == '_limit') {
-                limit = parseInt(value);
-                if (isNaN(limit) || limit < 0) throw BadRequestError(`invalid _limit ${value}: positive integer expected`);
-            } else if (key == '_sort') {
-                if (!(fields[value] && fields[value].can_sort)) {
-                    throw new BadRequestError(`invalid _sort key ${value}. Fields: ${ JSON.stringify(fields) }`);
-                }
-                sort = value;
-            } else if (fields[key] && fields[key].can_filter) {
-                const field = fields[key];
-                filter[key] = value;
-                if (field.match_regex) {
-                    $match[key] = { $regex: field.match_regex(value) }
-                } else if (field.match_integer) {
-                    $match[key] = parseInt(value);
-                } else if (field.match_ids) {
-                    $match['_id'] = {
-                        $in: value.split(",").map(id => new mongoose.Types.ObjectId(id))
-                    };
-                } else if (field.match_id_object) {
-                    $match[key] = mongoose.Types.ObjectId(value);
-                } else {
-                    $match[key] = value;
-                }
-            }
-        }
+        // attenzione all'output, nella pipeline ci sono oggetti che non 
+        // vengono serializzati correttamente:
+        // gli ObjectId sembrano stringhe,
+        // le RegExp sembrano oggetti vuoti
+        console.log(`${Model.modelName}.index PIPELINE ${JSON.stringify(pipeline)}`)
 
-        console.log($match);
+        const [ res ] = await Model.aggregate(pipeline)
 
-        let total, items;
-        const $sort = {};
-        $sort[sort] = direction;
-        const pipeline = [
-            {$match: restrict || {}},
-            {$match},
-            {$sort},
-            {$facet:{
-                "counting" : [ { "$group": {_id:null, count:{$sum:1}}} ],
-                "limiting" : [ { "$skip": 0}, {"$limit": limit} ]
-            }},
-            {$unwind: "$counting"},
-            {$project:{
-                total: "$counting.count",
-                items: "$limiting"
-            }}
-        ]
-        console.log(`Model ${Model.url} pipeline`, JSON.stringify(pipeline))
-        const res = await Model.aggregate(pipeline)
-        if (res.length === 0) {
-            total = 0;
-            items = res;
-        } else {
-            [{ total, items }] = res;
-        }
+        const items = res.items
+        const total = res.total
             
         if (populate !== undefined) {
             await Model.populate(items, { path: populate })
         }
         console.log(`${items.length} / ${total} items collected`);
 
-        return {
-            items,
-            limit,
-            sort,
-            direction,
-            filter,
-            total
-        };
+        return res
     },
 
     view: async (Model, id, { populate } = {}) => {
