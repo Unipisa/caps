efe3d514f33e9ab34ec0465f9d048dc2ae305f67
Leonardo Robol <leo@robol.it>
2024-09-25 18:33:03 +0200

    Release CAPS 2.12.4

--- backend/src/Application.php
+++ backend/src/Application.php
@@ -50,7 +50,7 @@ use Psr\Http\Message\ServerRequestInterface;
 class Application extends BaseApplication implements AuthenticationServiceProviderInterface
 {
     // Current CAPS version. This number is displayed in the web interface.
-    public static $_CAPSVERSION = '2.12.3';
+    public static $_CAPSVERSION = '2.12.4';
 
     /**
      * application version number
