c3a143ed2fe392acaaf24cb18e700128634b69ad
Leonardo Robol <leo@robol.it>
2025-05-22 12:23:08 +0200

    Settings, plus files I have forgotten.

--- frontend/src/pages/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage.tsx
@@ -1,19 +1,26 @@
 import React, {useState} from 'react'
-import { Card, Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import Card from '../components/Card'
 import Form from 'react-bootstrap/Form'
-import axios from 'axios'
+import HTMLEditor from '../components/HTMLEditor'
 
-import { useIndex } from '../modules/engine'
+
+import { useIndex, usePost } from '../modules/engine'
 import LoadingMessage from '../components/LoadingMessage'
 
 export default function SettingsPage({}) {
     const query = useIndex(['settings'], {})
+    const mutator = usePost(['settings'])
+
     if (query.isLoading) return <LoadingMessage>caricamento settings</LoadingMessage>
     const settings = query.data
+
     return <SettingsInternal settings={settings} submit={submit}/>
 
     async function submit(data) {
-        await axios.post('/api/v0/settings', data)
+        console.log("submit", data)
+        await mutator.mutate(data)
+        // await axios.post('/api/v0/settings', data)
         // query.mutate() // ERROR: query.mutate is not a function
     }
 }
@@ -24,60 +31,61 @@ function SettingsInternal({settings, submit}:{
 }) {
     const [data, setData] = useState(settings)
     return <div>
-        <h1>Impostazioni2</h1>
+        <h1>Impostazioni</h1>
         <Form>
-        <Card>
-            <Card.Header>
-                <Card.Title>Generali</Card.Title>
-            </Card.Header>
-            <Group label="Disclaimer">
-                <Description>Questa scritta, se non vuota, viene mostrata in cima ad ogni pagina</Description>
-                <textarea value={data.disclaimer} onChange={setter("disclaimer")}/>
-            </Group>
+        <Card title="Generali">
+        <Form.Group className="my-3">
+            <Form.Label>Disclaimer</Form.Label>                
+            <Form.Control value={data.disclaimer} onChange={setter("disclaimer")}/>
+            <Form.Text>Questa scritta, se non vuota, viene mostrata in cima ad ogni pagina.</Form.Text>
+        </Form.Group>
 
-            <Group label="Corso di studi">
-                <input value={data.cds} onChange={setter("cds")}/>
-            </Group>
+        <Form.Group className="my-3">
+            <Form.Label>Corso di studi</Form.Label>
+            <Form.Control value={data.cds} onChange={setter("cds")}/>
+        </Form.Group>
 
-            <Group label="Dipartimento">
-                <input value={data.department} onChange={setter("department")}/>
-            </Group>
+        <Form.Group className="my-3">
+            <Form.Label>Nome del dipartimento</Form.Label>
+            <Form.Control value={data.department} onChange={setter("department")}/>
+        </Form.Group>
             
-            <Group label="Istruzioni per l'utente">
-                <Description>Queste istruzioni vengono mostrate all'utente appena dopo il login, nella pagina
-                    dove sono visibili tutti i piani di studio presentati.
-                </Description>
-                <textarea value={data.userInstructions} onChange={setter("userInstructions")} />
-            </Group>
+        <Form.Group className="my-3">
+            <Form.Label>Istruzioni per l'utente</Form.Label>
+            <HTMLEditor content={data.userInstructions} setContent={(content) => setData({ ...data, "userInstructions": content })} />
+            <Form.Text>Queste istruzioni vengono mostrate all'utente appena dopo il login, nella pagina
+                dove sono visibili tutti i piani di studio presentati.
+                </Form.Text>
+        </Form.Group>
         </Card>
 
-        <Card>
-        <Card.Header>
-            <Card.Title>Piani di studio</Card.Title>
-        </Card.Header>
-        <Group label="Notifiche e-mail">
-            <Description>
+        <Card title="Piani di studio">
+        <Form.Group className="my-3">
+            <Form.Label>Notifiche e-mail</Form.Label>
+            <Form.Control value={data.notifiedEmails} onChange={setter("notifiedEmails")}/>
+            <Form.Text>
                 Questo campo contiene una lista di indirizzi e-mail, separati da virgole, che vengono
                 notificati ad ogni nuova sottomissione e approvazione di un piano di studio.
-            </Description>
-            <input value={data.notifiedEmails} onChange={setter("notifiedEmails")}/>
-        </Group> 
+            </Form.Text>
+        </Form.Group> 
 
-        <Group label="Firma per i piani">
-            <Description>Questa firma viene apposta su ogni piano approvato, quando si
+        <Form.Group className='my-3'>
+            <Form.Label>Firma per i piani</Form.Label>
+            <Form.Control value={data.approvalSignatureText} onChange={setter("approvalSignatureText")}/>
+            <Form.Text>Questa firma viene apposta su ogni piano approvato, quando si
             seleziona il pulsante "Stampa piano".
-            </Description>
-            <input value={data.approvalSignatureText} onChange={setter("approvalSignatureText")}/>
-        </Group>
+            </Form.Text>
+        </Form.Group>
 
-        <Group label="Nome dei file .pdf">
-            <Description>È possibile configurare il nome per i file .pdf che vengono
+        <Form.Group>
+            <Form.Label>Nome dei file .pdf</Form.Label>
+            <Form.Control value={data.pdfName} onChange={setter("pdfName")}/>
+            <Form.Text>È possibile configurare il nome per i file .pdf che vengono
                 scaricati cliccando su "Scarica in PDF" nella pagina di un piano di studio. Le stringhe '%d', '%s',
                 '%n' e '%c' vengono sostituite con la data di ultima modifica, il cognome, il nome, e il curriculum,
                 rispettivamente. Non è necessario includere l'estensione .pdf.
-            </Description>
-            <input value={data.pdfName} onChange={setter("pdfName")}/>
-        </Group>
+            </Form.Text>
+        </Form.Group>
         </Card>
         <Button onClick={() => submit(data)}>Salva impostazioni</Button>
         </Form>
