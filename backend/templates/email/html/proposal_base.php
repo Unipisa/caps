e8eb8a9c53589f9598d79b15e94d7ae929b7d5c5
CDP21 <132059856+c2d13p@users.noreply.github.com>
2026-05-27 15:09:37 +0000

    remove note label from email template

--- backend/templates/email/html/proposal_base.php
+++ backend/templates/email/html/proposal_base.php
@@ -162,9 +162,6 @@ Anno Accademico <?= $year ?>/<?=  ($year + 1) ?><br>
 <?php if (!empty($proposal['note'])): ?>
 <div>
     <h3>Nota</h3>
-    <?php if (!empty($proposal['curriculum']['degree']['note_label'])): ?>
-        <p><?= $proposal['curriculum']['degree']['note_label'] ?></p>
-    <?php endif; ?>
     <p style="white-space: pre-wrap;"><?= h($proposal['note']) ?></p>
 </div>
 <?php endif; ?>
